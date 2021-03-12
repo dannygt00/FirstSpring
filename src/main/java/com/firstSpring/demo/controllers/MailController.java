fb36722dd1166646ad364078c4a89c0ce045212f
danielramirez <danielramirez@localhost>
2021-03-12 12:43:31 -0500

    Beginning of project

---
+++ src/main/java/com/firstSpring/demo/controllers/MailController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.firstSpring.demo.controllers;
+
+import com.firstSpring.demo.mail.MailSender;
+import com.firstSpring.demo.mail.MockMailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author danielramirez
+ */
+@RestController
+public class MailController {
+    
+    
+    private MailSender mailSender; 
+    
+    @Autowired
+    public void MailController(MailSender smtp) {
+        this.mailSender = smtp;
+    }
+    
+    
+    
+    
+     @RequestMapping("/mail")
+    public String mail(){
+        
+        mailSender.send("mail@example.com","test mail", "test body");
+        
+        return "Mail Sent!";
+    }
+}
---
+++ src/main/java/com/firstSpring/demo/mail/SmtpMailSender.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.firstSpring.demo.mail;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author danielramirez
+ */
+@Component
+public class SmtpMailSender implements MailSender {
+
+    private static Log Log = LogFactory.getLog(SmtpMailSender.class);
+
+    @Override
+    public void send(String to, String subject, String body){
+        Log.info("Sending SMTP Mail to "+ to);
+        Log.info("with subject: "+ subject);
+        Log.info("and body: "+ body);
+
+    }
+
+}
